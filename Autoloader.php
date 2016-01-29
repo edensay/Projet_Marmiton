8426295753cf1b89fd701b5eb4f14bef77a08228
Maxime Margaine <maxime@hippocampe.fr>
2016-01-29 15:23:31 +0100

    Modification de l'autoloader

--- Autoloader.php
+++ Autoloader.php
@@ -6,40 +6,72 @@
  * Time: 22:22
  */
 
-/**
- * Class Autoloader
- */
 namespace App {
     class Autoloader
     {
-        /**
-         * Enregistre notre autoloader
-         */
         static function register()
         {
-            spl_autoload_register(array(__CLASS__, 'autoload'));
+            spl_autoload_register(array(__CLASS__, '_autoloadController'));
+            spl_autoload_register(array(__CLASS__, '_autoloadModel'));
+            spl_autoload_register(array(__CLASS__, '_autoloadView'));
         }
 
-        /**
-         * Inclue le fichier correspondant à notre classe
-         * @param $class string Le nom de la classe à charger
-         */
-        static function autoload($class)
-        {
+        static function _autoloadController($class) {
             $dir = explode('\\', $class);
             $path = __DIR__ ."/".$dir[0];
+
             if(is_dir(realpath($path))) {
-                $files = explode('_', preg_replace('/(?<=\\w)(?=[A-Z])/',"_$1", $dir[1]));
-                $path .= "/".$files[1]."/".$dir[1].'.php';
-                if(file_exists(realpath($path))) {
-                    require_once(realpath($path));
+                if(strpos($dir[1], 'Controller')) {
+                    $path .= "/Controller/".$dir[1].'.php';
+
+                    if (file_exists($path)) {
+                        require_once(realpath($path));
+                    } else {
+                        echo "Le fichier ${dir[1]} n'existe pas dans le namespace ${dir[0]}";
+                        return true;
+                    }
                 } else {
-                    echo "Le fichier $dir[1] n'existe pas dans le namespace $dir[0]";
-                    exit();
+                    return false;
                 }
             } else {
-                echo "Le Namespace $dir[0] n'existe pas !";
-                exit();
+                echo "Le Namespace ${dir[0]} n'existe pas !";
+                return true;
+            }
+        }
+
+        static function _autoloadModel($class) {
+            $dir = explode('\\', $class);
+            $path = __DIR__ ."/".$dir[0];
+
+            if(strpos($dir[1], 'Model')) {
+                $path .= "/Model/".$dir[1].'.php';
+
+                if (file_exists($path)) {
+                    require_once(realpath($path));
+                } else {
+                    echo "Le fichier ${dir[1]} n'existe pas dans le namespace ${dir[0]}";
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        static function _autoloadView($class) {
+            $dir = explode('\\', $class);
+            $path = __DIR__ ."/".$dir[0];
+            
+            if(strpos($dir[1], 'View')) {
+                $path .= "/View/".$dir[1].'.php';
+
+                if (file_exists($path)) {
+                    require_once(realpath($path));
+                } else {
+                    echo "Le fichier ${dir[1]} n'existe pas dans le namespace ${dir[0]}";
+                    return true;
+                }
+            } else {
+                return false;
             }
         }
     }
