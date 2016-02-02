ae6797eb8e6dffbf8cfc822587a0ca95236d2835
maltuin <maxime.margaine@yahoo.fr>
2016-02-02 23:47:47 +0100

    Ajout des formulaires

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -17,9 +17,10 @@ class PageController extends BaseController{
      * @param string $name Name of the page
      */
     function index($name = 'Default'){
+        require(APP.DS.'form'.DS.'RecetteForm.php');
         $form = new RecetteForm();
         $this->set('titrePage', 'Nom de ma page : '.$name);
-        $this->set(array('nom' => $name, 'contenu' => 'paragraphe', "form" =>$form->render()));
+        $this->set(array('nom' => $name, 'contenu' => 'paragraphe', 'form' => $form->render()));
         $this->render('index');
     }
 
--- app/form/RecetteForm.php
+++ app/form/RecetteForm.php
@@ -5,6 +5,7 @@
  * Date: 02/02/2016
  * Time: 23:32
  */
+require(APP.DS.'form'.DS.'BaseForm.php');
 class RecetteForm extends BaseForm
 {
     function __construct()
