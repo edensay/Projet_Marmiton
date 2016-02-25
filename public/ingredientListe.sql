991a66e011446876a750fa3ef51a79b75650a116
maltuin <maxime.margaine@yahoo.fr>
2016-02-25 13:45:52 +0100

    add sql

---
+++ public/ingredientListe.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Jeu 25 Février 2016 à 13:45
+-- Version du serveur: 5.5.38
+-- Version de PHP: 5.5.17-1~dotdeb.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `marmiton`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredientListe`
+--
+
+CREATE TABLE IF NOT EXISTS `ingredientListe` (
+  `idil` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  PRIMARY KEY (`idil`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
