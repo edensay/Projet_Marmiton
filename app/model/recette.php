3c442a314521166630757f4a2e83e4f244a4c7c7
maltuin <margai_m@etna-alternance.net>
2016-02-25 20:33:30 +0000

    Ajout de la requette detail

--- app/model/recette.php
+++ app/model/recette.php
@@ -32,28 +32,16 @@ class recette
     public function getRecette($id)
     {
         $PDO = myPdo::getInstance();
-        $recette = $PDO->prepare("SELECT titre, temps texte, img ,
-                                    (SELECT GROUP_CONCAT( B.nom
-                                        SEPARATOR  ', ' ) 
-                                        FROM ingredients A
-                                        LEFT JOIN ingredientListe B ON A.idi = B.idil
-                                        WHERE idr = 1
-                                    ) AS ingredients,
-                                    (SELECT GROUP_CONCAT( quantité
-                                        SEPARATOR  ', ' ) 
-                                        FROM ingredients
-                                        WHERE idr = 1
-                                    ) AS quantités,
-                                    (SELECT GROUP_CONCAT( B.nom
-                                        SEPARATOR  ', ' ) 
-                                        FROM tags A
-                                        LEFT JOIN tagList B ON A.idt = B.idt
-                                        WHERE idr = :id
-                                    ) as tags
-                                    FROM recettes
-                                    WHERE idr = 1");
+        $PDO->exec("SET CHARACTER SET utf8");
+        $query = "SELECT R.titre, R.temps, R.texte, R.img ,
+                    (SELECT GROUP_CONCAT( B.nom SEPARATOR  ', ' ) FROM ingredients A LEFT JOIN ingredientListe B ON A.idi = B.idil WHERE idr = :id ) AS ingredients,
+                    (SELECT GROUP_CONCAT( quantité SEPARATOR  ', ' ) FROM ingredients WHERE idr = :id) AS quantites,
+                    (SELECT GROUP_CONCAT( B.nom SEPARATOR  ', ' ) FROM tags A LEFT JOIN tagList B ON A.idt = B.idt WHERE idr = :id ) as tags
+                    FROM recettes R
+                    WHERE idr = :id";
+        $recette = $PDO->prepare($query);
         $recette->execute(array(':id' => $id));
-        var_dump($recette->fetchAll());
+        //var_dump($recette->fetchAll());
         return $recette->fetchAll();
     }
 }
